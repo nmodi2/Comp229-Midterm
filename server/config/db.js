c8a0f8b0b17f6434c1d62561d73e8764b3cc4e4d
nmodi2 <79933465+nmodi2@users.noreply.github.com>
2021-10-25 22:26:15 -0400

    Updated db URI to work with Mongodb Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/book-store"
-};
+ // "URI": "mongodb://localhost/book-store"
+  "URI": "mongodb+srv://nishit:z7R6tjAGAg43vD2G@mongodb.vv3y9.mongodb.net/book-store?retryWrites=true&w=majority"
+}
